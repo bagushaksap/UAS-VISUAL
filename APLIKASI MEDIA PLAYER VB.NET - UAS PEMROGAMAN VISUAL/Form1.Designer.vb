501871c796bbeeba366481788d44029a1461e25c
bagushaksap <104568053+bagushaksap@users.noreply.github.com>
2023-02-18 00:53:43 +0700

    Add files via upload

---
+++ APLIKASI MEDIA PLAYER VB.NET - UAS PEMROGAMAN VISUAL/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblJudul = New System.Windows.Forms.Label()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnPause = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnReplay = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnPrev = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnLoadPlaylist = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblJudul
+        '
+        Me.lblJudul.AutoSize = True
+        Me.lblJudul.Font = New System.Drawing.Font("League Spartan", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJudul.Location = New System.Drawing.Point(163, 18)
+        Me.lblJudul.Name = "lblJudul"
+        Me.lblJudul.Size = New System.Drawing.Size(252, 26)
+        Me.lblJudul.TabIndex = 1
+        Me.lblJudul.Text = "MEDIA PLAYER VB.NET"
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(16, 352)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 2
+        Me.btnBrowse.Text = "BROWSE"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Location = New System.Drawing.Point(97, 352)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(75, 23)
+        Me.btnPlay.TabIndex = 3
+        Me.btnPlay.Text = "PLAY"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnPause
+        '
+        Me.btnPause.Location = New System.Drawing.Point(178, 352)
+        Me.btnPause.Name = "btnPause"
+        Me.btnPause.Size = New System.Drawing.Size(75, 23)
+        Me.btnPause.TabIndex = 4
+        Me.btnPause.Text = "PAUSE"
+        Me.btnPause.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(259, 352)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 23)
+        Me.btnStop.TabIndex = 5
+        Me.btnStop.Text = "STOP"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnReplay
+        '
+        Me.btnReplay.Location = New System.Drawing.Point(340, 352)
+        Me.btnReplay.Name = "btnReplay"
+        Me.btnReplay.Size = New System.Drawing.Size(75, 23)
+        Me.btnReplay.TabIndex = 6
+        Me.btnReplay.Text = "REPLAY"
+        Me.btnReplay.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(502, 352)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(75, 23)
+        Me.btnNext.TabIndex = 7
+        Me.btnNext.Text = ">>"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnPrev
+        '
+        Me.btnPrev.Location = New System.Drawing.Point(421, 352)
+        Me.btnPrev.Name = "btnPrev"
+        Me.btnPrev.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrev.TabIndex = 8
+        Me.btnPrev.Text = "<<"
+        Me.btnPrev.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Lucida Sans Typewriter", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 17
+        Me.ListBox1.Location = New System.Drawing.Point(604, 59)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(247, 344)
+        Me.ListBox1.TabIndex = 9
+        '
+        'btnLoadPlaylist
+        '
+        Me.btnLoadPlaylist.Font = New System.Drawing.Font("League Spartan", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLoadPlaylist.Location = New System.Drawing.Point(604, 9)
+        Me.btnLoadPlaylist.Name = "btnLoadPlaylist"
+        Me.btnLoadPlaylist.Size = New System.Drawing.Size(247, 35)
+        Me.btnLoadPlaylist.TabIndex = 10
+        Me.btnLoadPlaylist.Text = "LOAD PLAYLIST"
+        Me.btnLoadPlaylist.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Lucida Bright", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(268, 393)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 23)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Volume"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.LargeChange = 100
+        Me.TrackBar1.Location = New System.Drawing.Point(421, 393)
+        Me.TrackBar1.Maximum = 100
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(156, 45)
+        Me.TrackBar1.TabIndex = 12
+        Me.TrackBar1.Value = 50
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        Me.OpenFileDialog2.Multiselect = True
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(16, 59)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(561, 287)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MistyRose
+        Me.ClientSize = New System.Drawing.Size(877, 450)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnLoadPlaylist)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.btnPrev)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnReplay)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnPause)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.lblJudul)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents lblJudul As Label
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents btnPlay As Button
+    Friend WithEvents btnPause As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents btnReplay As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnPrev As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btnLoadPlaylist As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+End Class
---
+++ APLIKASI MEDIA PLAYER VB.NET - UAS PEMROGAMAN VISUAL/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AxWindowsMediaPlayer1.uiMode = "None"
+        AxWindowsMediaPlayer1.settings.volume = TrackBar1.Value
+        Label1.Text = "Volume : " & TrackBar1.Value & ""
+    End Sub
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        OpenFileDialog1.ShowDialog()
+        AxWindowsMediaPlayer1.URL = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+    End Sub
+
+    Private Sub btnPause_Click(sender As Object, e As EventArgs) Handles btnPause.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+    End Sub
+
+    Private Sub btnReplay_Click(sender As Object, e As EventArgs) Handles btnReplay.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.currentPosition = Val(AxWindowsMediaPlayer1.Ctlcontrols.currentPosition) = 0
+    End Sub
+
+    Private Sub btnPrev_Click(sender As Object, e As EventArgs) Handles btnPrev.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.currentPosition = Val(AxWindowsMediaPlayer1.Ctlcontrols.currentPosition) - 10
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.currentPosition = Val(AxWindowsMediaPlayer1.Ctlcontrols.currentPosition) + 10
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        AxWindowsMediaPlayer1.settings.volume = TrackBar1.Value
+        Label1.Text = "Volume : " & TrackBar1.Value & ""
+    End Sub
+
+    Dim paths As String()
+    Dim filenames As String()
+
+    Private Sub btnLoadPlaylist_Click(sender As Object, e As EventArgs) Handles btnLoadPlaylist.Click
+        If (OpenFileDialog2.ShowDialog = DialogResult.OK) Then
+            filenames = OpenFileDialog2.SafeFileNames
+            paths = OpenFileDialog2.FileNames
+            For i As Integer = 0 To filenames.Length - 1
+                ListBox1.Items.Add(filenames(i))
+            Next
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        AxWindowsMediaPlayer1.URL = paths(ListBox1.SelectedIndex)
+    End Sub
+End Class
